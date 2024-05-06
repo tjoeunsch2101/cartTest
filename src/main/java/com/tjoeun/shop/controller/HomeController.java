2e468a8c31d17eef9683597c390a9cbb0799c7a8
lew <bit4math@gmail.com>
2024-05-06 19:10:43 +0900

    url pattern / 이면 index.html 을 찾아감

--- src/main/java/com/tjoeun/shop/controller/HomeController.java
+++ src/main/java/com/tjoeun/shop/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	// url pattern / 이면 index.html 을 찾아감
 	@GetMapping("/")
 	public String home() {
 		return "index";
